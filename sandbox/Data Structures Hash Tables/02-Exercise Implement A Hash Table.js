535c6c92f5cd1cce426b7ef3b0f11c9e0ea0547e
Nguyen Thanh Nhan <nhan2151@gmail.com>
2020-03-12 12:45:18 +0700

    complete hash table exercise

--- sandbox/Data Structures Hash Tables/02-Exercise Implement A Hash Table.js
+++ sandbox/Data Structures Hash Tables/02-Exercise Implement A Hash Table.js
@@ -3,18 +3,43 @@ class HashTable {
     this.data = new Array(size);
   }
 
+  // Hash function => O(1)
   _hash(key) {
     let hash = 0;
-    for (let i = 0; i < key.lenth; i++) {
+    for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * i) % this.data.length;
     }
-
     return hash;
   }
 
-  set(key, value) {}
+  // O(1)
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  // O(1)
+  // In the bad case time complexity will O(n)
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
-const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000);
-myHashTable.get('grapes');
+const myHashTable = new HashTable(30);
+myHashTable.set("grapes", 10000);
+myHashTable.set("apples", 54);
+console.log(myHashTable.get("grapes"));
+console.log(myHashTable.get("apples"));
