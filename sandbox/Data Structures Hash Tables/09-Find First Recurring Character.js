79f6b76f76a4be84b2ed816cb13042fa39239fd9
NhanNguyen001 <nhan2151@gmail.com>
2020-03-12 21:57:09 +0700

    complete hash tables

--- sandbox/Data Structures Hash Tables/09-Find First Recurring Character.js
+++ sandbox/Data Structures Hash Tables/09-Find First Recurring Character.js
@@ -12,10 +12,31 @@
 // [2,5,5,2,3,5,1,2,4]
 // return 5 because the pairs are before 2,2
 
-function firstRecurringCharacter(input) {
-  for (let i = 0; i < input.length; i++) {
-    for(let j=1; i< input.length; j++)
-  }
-}
+// function firstRecurringCharacter(input) {
+//   for (let i = 0; i < input.length; i++) {
+//     for (let j = i + 1; i < input.length; j++) {
+//       if (input[i] === input[j]) {
+//         return input[i];
+//       }
+//     }
+//   }
 
-firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]);
+//   return undefined;
+// } // O(n^2)
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+
+  return undefined;
+} // O(n)
+
+// console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter2([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter2([2, 1, 1, 2, 3, 5, 1, 2, 4]));
