c9c015ad8d9420118574bad73ba4b5e5c26ec3bd
Nguyen Thanh Nhan <nhan2151@gmail.com>
2020-03-24 12:19:10 +0700

    Complete write first linked list

---
+++ sandbox/Data Structures Linked Lists/02-Our First Linked List.js
@@ -0,0 +1,44 @@
+// 10-- > 5-- > 16;
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    // Code here
+    const newNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value)
+}
+
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList.append(5));
+console.log(myLinkedList.append(16));
+// console.log(myLinkedList);
